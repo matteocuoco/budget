b8d82428c116a1c862fc629ef0a8b4ba872ad758
MasterchefUnited <42892973+MasterchefUnited@users.noreply.github.com>
2019-02-18 23:03:43 +0100

    Updates

---
+++ precache-manifest.53262fa354e393f367053940f9feab3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce161922f64a0ecf538a",
+    "url": "/budget/static/js/runtime~main.ce161922.js"
+  },
+  {
+    "revision": "19d995ee54e61732be41",
+    "url": "/budget/static/js/main.19d995ee.chunk.js"
+  },
+  {
+    "revision": "1bc9c3083a25ac137117",
+    "url": "/budget/static/js/2.1bc9c308.chunk.js"
+  },
+  {
+    "revision": "19d995ee54e61732be41",
+    "url": "/budget/static/css/main.9d81bcf7.chunk.css"
+  },
+  {
+    "revision": "b008fb691acef9102fcd01d5cb9c3a46",
+    "url": "/budget/index.html"
+  }
+];
